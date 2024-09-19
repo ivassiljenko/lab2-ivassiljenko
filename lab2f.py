510047c3de1968a3455a46e3a2f5465bb76bcdc6
Ivan Vassiljenko <ivassiljenko@myseneca.ca>
2024-09-19 11:20:28 -0400

    added lab2f

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Author: [Ivan Vassiljenko]
+# Author ID: [ivassiljenko]
+# Date Created: [2024/09/19]
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer -1
+
+print("blast off!")
